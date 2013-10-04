b338d4d1844f2de71732282cd97c6c4b93b9bd03
Jose G. Coronado <github@coronadoland.com>
2013-10-04 13:48:18 +0200

    Removing min.versions

--- js/service-connector.js
+++ js/service-connector.js
@@ -1,6 +1,6 @@
 /*!
  * service-connector.js
- * https://github.com/coronadoland
+ * http://coronadoland.com/service-connector
  *
  * Copyright (c) 2011-2013 Jose Antonio Garcia
  * Released under the MIT license
@@ -30,7 +30,7 @@
 					data += ']';
 				} else if (type == "string") {
 					data += (data==''?'':',') + '"'+property+'":"'+value+'"';
-				} else if (type == "number") {
+				} else if (type == "number" || type == "boolean") {
 					data += (data==''?'':',') + '"'+property+'":'+value;
 				} else if (type == "object" && value !== null) {
 					data += (data==''?'':',') + '"'+property+'":'+stringfy(value);
@@ -55,7 +55,8 @@
 
 				return $.ajax({
 				    contentType: contentType,
-				    data: data?stringfy(data):'{}',
+//				    data: data?stringfy(data):'{}',
+				    data: data?JSON.stringify(data):'{}',
 				    dataType: dataType,
 				    type: type,
 				    url: url
@@ -67,6 +68,7 @@
 		var send = function(parms, dataType, data1, callback, element) {
 			$.when(ajax('POST', dataType, parms.service.url, parms.service.restful, data1), parms.panel.waiter(element)).then(
 				function(data2) {
+					if (parms.panel &&  parms.panel.success) parms.panel.success();
 					if (parms.service.property) {
 						callback(data2[0][parms.service.property]);
 					} else {
@@ -103,52 +105,96 @@
 
 			var template = null;
 			if (parms.template) {
-				var templateId = parms.template.id;
- 				template = templateId +'TPL';
-				$('#' + templateId).template(template);
+				_.templateSettings.variable = "rc";
+				
+				if (parms.template.id) {
+					var templateId = parms.template.id;
+ 					template = _.template($('#'+templateId).html());
+ 				}
+				
 			}
 
 			var $this = this;
-
-			this.on('LOAD', function(e, data1) {
+			
+			this.on('LOAD', function(e, data1, parmsexec) {
 				if (parms.service) {
-					send(parms, (parms.template || parms.then)?'json':'text', data1, function(data2) {
+				
+					var type = null;
+					if (parms.template) {
+						type = 'json';
+					} else if (parms.then) {
+						type = 'json';
+					} else if (parmsexec && parmsexec.then) {
+						type = 'json';
+					} else {
+						type = 'text';
+					}					
+				
+					var element = null;
+					if (parmsexec && parmsexec.element) {element = parmsexec.element}
+					send(parms, type, data1, function(data2) {
 						if (parms.then) {
 							parms.then(data2);
 						} else if (parms.template) {
 							$this.trigger('LOADTEMPLATE', [data2]);
+						} else if (parmsexec && parmsexec.then) {
+						 	parmsexec.then(data2)
 						} else {
 							$this.html('');
 							$this.append(data2);
 						}
-					});
-				} else if (parms.template){
-					$.tmpl( parms.template.id, data1 ).appendTo( $this );
+					}, element);
+					
+//				} else if (parms.template){
+//					$.tmpl( parms.template.id, data1 ).appendTo( $this );
 				}
 			});
-			
-			var nofirsttime = false;
-			this.on('LOADTEMPLATE', function(e, data) {
 
-				var show = function() {
-					$this.html('');
-					if (data) $this.append($.tmpl(template, data));
-					if (parms.template.shower) parms.template.shower($this);
-					nofirsttime = true;
-				};
+			if (parms.template) {
+			
+				var nofirsttime = false;
 				
-				if (parms.template.remover && nofirsttime) {
-					$.when(parms.template.remover($this)).then(show);
-				} else {
-					show();
-				}
 				
-			});
+				this.on('LOADTEMPLATE', function(e, data) {
+				
+					var show = function() {
+						$this.html('');
+						if (data) $this.append(template(data));
+						if (parms.template && parms.template.shower) parms.template.shower($this);
+						nofirsttime = true;
+					};
+				
+					var show2 = function() {
+						if (parms.template && parms.template.remover && nofirsttime) {
+							$.when(parms.template.remover($this)).then(show);
+						} else {
+							show();
+						}
+					};
+				
+					if (parms.template.url && template==null) {
+						$.ajax({
+				   	        url: parms.template.url, method: 'GET', dataType: 'text'
+				        }).then(function(data) {
+				        	template = _.template(data);
+				        	show2();
+				        }).fail(function(XMLHttpRequest, textStatus, errorThrown) {
+							if (window.console) console.log(textStatus);
+						});
+
+					} else {
+						show2();
+					}
+				
+				
+				});
+				
+			}
 
 		};
 		
-		jQuery.fn.load = function(parms)         {this.trigger('LOAD',[parms]);};
-		jQuery.fn.loadTemplate = function(parms) {this.trigger('LOADTEMPLATE',[parms]);};
+		jQuery.fn.load = function(data1, data2) {this.trigger('LOAD',[data1, data2]);};
+		jQuery.fn.loadTemplate = function(data) {this.trigger('LOADTEMPLATE',[data]);};
 
 		
 		jQuery.fn.sender = function(parms) {
@@ -163,7 +209,54 @@
 			});
 		};
 		
-		jQuery.fn.send = function(parms, element) {this.trigger('SEND',[parms, element]);};
+		jQuery.fn.send = function(data1, data2) {this.trigger('SEND',[data1, data2]);};
+		
+		
+		jQuery.fn.loadScript = function(parms) {
+	
+			var $deferreds = [];
+			var $this = this;
+			
+  			if (parms.templates) {
+				var index = 0;
+  				$.each(parms.templates, function(index, template) {
+		 		    var dfd = new jQuery.Deferred();
+		 			$deferreds[index++] = dfd;
+
+	 				$.ajax({
+			   	        url: template.url, method: 'GET', dataType: 'text'
+			        }).then(
+           				function (data) {
+           					if (template.name) {
+							    $('head').append('<script id="' + template.name + '" type="text/template">' + data + '</script>');
+           					} else {
+								$this.html(data);	// Error
+   							}        					
+        	       			dfd.resolve();
+					}).fail(function(XMLHttpRequest, textStatus, errorThrown) {
+						dfd.fail(XMLHttpRequest, textStatus, errorThrown);
+					});
+  				});
+  			} else {
+  			
+ 	 		    var dfd = new jQuery.Deferred();
+	 			$deferreds[0] = dfd;
+	 			
+	 			$.ajax({
+			   	   url: parms.url, method: 'GET', dataType: 'text'
+			    }).then(function (data) {
+					$this.html(data);
+	   	       		dfd.resolve();
+				}).fail(function(XMLHttpRequest, textStatus, errorThrown) {
+					dfd.fail(XMLHttpRequest, textStatus, errorThrown);
+				});
+				
+  			}
+  				
+			return $.when.apply(null, $deferreds);
+		    
+		}
+        
 		
 	});
 }(window.jQuery);
